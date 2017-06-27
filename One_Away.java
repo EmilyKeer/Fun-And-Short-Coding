85af2f5446f724cf4d506c62066adbcc51930108
Keer <EmilyKeer@users.noreply.github.com>
2017-06-26 22:30:36 -0400

    Update One_Away.java

--- One_Away.java
+++ One_Away.java
@@ -1,5 +1,6 @@
 /*
-There are three types of edits that can be performed on strings: insert a character, remove a character and replace a character. Given two strings, write a function to check if they are within one edit of each other.
+There are three types of edits that can be performed on strings: insert a character, remove a character and replace 
+a character. Given two strings, write a function to check if they are within one edit of each other.
 */
 //Keer's solution O(n)
 /*
