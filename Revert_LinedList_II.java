3a202ef804fd4a8eed46171f6f849d30a11c4bab
Keer <EmilyKeer@users.noreply.github.com>
2017-06-28 21:42:18 -0400

    Update Revert_LinedList_II.java

--- Revert_LinedList_II.java
+++ Revert_LinedList_II.java
@@ -69,19 +69,22 @@ public class Solution {
  * }
  */
 public class Solution {
+    /*
+    1.find premNode
+    2.track mNode, let nNode=mNode, put postnNode
+    3.for loop move nNode postnNode
+    */
     public ListNode reverseBetween(ListNode head, int m, int n) {
+        //empty list and boundary cases
         if (m >= n || head == null) {
             return head;
         }
-
+        //if m == 1
         ListNode dummy = new ListNode(0);
         dummy.next = head;
         head = dummy;
 
         for (int i = 1; i < m; i++) {
-            if (head == null) {
-                return null;
-            }
             head = head.next;
         }
 
@@ -89,9 +92,6 @@ public class Solution {
         ListNode mNode = head.next;
         ListNode nNode = mNode, postnNode = mNode.next;
         for (int i = m; i < n; i++) {
-            if (postnNode == null) {
-                return null;
-            }
             ListNode temp = postnNode.next;
             postnNode.next = nNode;
             nNode = postnNode;
